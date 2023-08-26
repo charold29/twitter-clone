5d51659c7c8989d9fec368a3e6242a80d9cccb66
Harold Rojas <harolrop14@gmail.com>
2023-08-25 21:02:33 -0500

    page with policies for get all users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home () {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export default async function Home () {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: posts } = await supabase.from('posts').select('*')
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       Hi Twitter 👋
+      <pre>
+        {
+          JSON.stringify(posts, null, 2)
+        }
+      </pre>
     </main>
   )
 }
